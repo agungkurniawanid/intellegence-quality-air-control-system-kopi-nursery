161eaf4087ed40adef2e8c7f5d114e9ca4a2b7a3
Agung Kurniawan <agungklewang26@gmail.com>
2024-11-15 04:26:24 +0700

    done functional forgot password, send otp or resend otp

--- lib/constants/api_constant.dart
+++ lib/constants/api_constant.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // main url
-  static const String baseUrl = 'https://nursery-mbkm.research-ai.my.id';
+  static const String baseUrl = 'http://192.168.1.26:8000';
 
   // endpoint
   static const String loginEndpoint = '$baseUrl/api/login';
@@ -9,6 +9,11 @@ class ApiConstants {
   static const String getDataTempHumidityEndpoint = '$baseUrl/api/getdataalat/';
   static const String fotoProfilPath = '/foto_profil/';
   static const String pompaToggleEndpoint = '$baseUrl/api/aturpompa';
+  static const String lupaPasswordEndpoint = '$baseUrl/api/lupa-password';
+  static const String verifikasiOtpEndpoint =
+      '$baseUrl/api/lupa-password/verifikasi-otp/';
+  static const String kirimUlangOtpEndpoint =
+      '$baseUrl/api/lupa-password/kirim-ulang-otp/';
 
   // api external key
   static const String openWeatherApiKey = '89c748adb995fb8bc8afbe287c41ed51';
--- lib/screens/reset_password.dart
+++ lib/screens/reset_password.dart
@@ -4,115 +4,137 @@ import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iqacs/screens/login_screen.dart';
 
-class ResetPassword extends ConsumerWidget {
-  const ResetPassword({super.key});
+class ResetPassword extends ConsumerStatefulWidget {
+  final String noTelfon;
+  const ResetPassword({super.key, required this.noTelfon});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ResetPasswordState createState() => ResetPasswordState();
+}
+
+class ResetPasswordState extends ConsumerState<ResetPassword> {
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image(
-                  image: const AssetImage("assets/icons/newpassword.png"),
-                  width: MediaQuery.of(context).size.width,
-                  fit: BoxFit.cover,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image(
+                image: const AssetImage("assets/icons/newpassword.png"),
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
+              ),
+              const Gap(10.0),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Text(
+                  "Reset\nPassword?",
+                  style: GoogleFonts.poppins(
+                      fontSize: 38.0, fontWeight: FontWeight.w500),
                 ),
-                const Gap(10.0),
-                Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Text(
-                    "Reset\nPassword?",
-                    style: GoogleFonts.poppins(
-                        fontSize: 38.0, fontWeight: FontWeight.w500),
+              ),
+              const Gap(10.0),
+              Padding(
+                padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                child: TextField(
+                  controller: _newPasswordController,
+                  keyboardType: TextInputType.text,
+                  style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20),
+                  decoration: InputDecoration(
+                    hintText: "New password",
+                    hintStyle:
+                        GoogleFonts.poppins(color: const Color(0xFF6C798F)),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(width: 2, color: Colors.black),
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 2, color: Color(0xFFDFE1E5)),
+                    ),
                   ),
                 ),
-                const Gap(10.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-                  child: TextField(
-                    keyboardType: TextInputType.text,
-                    style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 20),
-                    decoration: InputDecoration(
-                      hintText: "New password",
-                      hintStyle:
-                          GoogleFonts.poppins(color: const Color(0xFF6C798F)),
-                      enabledBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(width: 2, color: Colors.black),
+              ),
+              const Gap(14.0),
+              Padding(
+                padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                child: TextField(
+                  controller: _confirmPasswordController,
+                  keyboardType: TextInputType.text,
+                  style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20),
+                  decoration: InputDecoration(
+                    hintText: "Confirm password",
+                    hintStyle:
+                        GoogleFonts.poppins(color: const Color(0xFF6C798F)),
+                    enabledBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(width: 2, color: Colors.black),
+                    ),
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(width: 2, color: Color(0xFFDFE1E5)),
+                    ),
+                  ),
+                ),
+              ),
+              const Gap(14.0),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      if (_newPasswordController.text ==
+                          _confirmPasswordController.text) {
+                        // Add your reset password logic here
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const LoginScreen(title: 'Login'),
+                          ),
+                        );
+                      } else {
+                        // Show a message indicating that passwords do not match
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 8),
+                      fixedSize: const Size.fromHeight(60),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 2, color: Color(0xFFDFE1E5)),
+                    ),
+                    label: Text(
+                      "Submit Perubahan",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
                   ),
                 ),
-                const Gap(14.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-                  child: TextField(
-                    keyboardType: TextInputType.text,
-                    style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 20),
-                    decoration: InputDecoration(
-                      hintText: "Confirm password",
-                      hintStyle:
-                          GoogleFonts.poppins(color: const Color(0xFF6C798F)),
-                      enabledBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(width: 2, color: Colors.black),
-                      ),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(width: 2, color: Color(0xFFDFE1E5)),
-                      ),
-                    ),
-                  ),
-                ),
-                const Gap(14.0),
-                Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const LoginScreen(title: 'Login')));
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.black,
-                          foregroundColor: Colors.black,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 16, vertical: 8),
-                          fixedSize: const Size.fromHeight(60),
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        label: Text(
-                          "Submit Perubahan",
-                          style: GoogleFonts.poppins(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    )),
-              ],
-            ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
